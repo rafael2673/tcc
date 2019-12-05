97a6ba3edba9218682a43c30e6b9507adcab276f
Rafael <rafaelribeirofranco4@gmail.com>
2019-12-05 10:44:55 -0300

    Adição de ajax

---
+++ SiteArtes/app/models/Select2VO.java
@@ -0,0 +1,17 @@
+package models;
+
+public class Select2VO {
+
+	public Long id;
+	public String text;
+
+	public Select2VO() {
+	
+	}
+	
+	public Select2VO(Long id, String text) {
+		this.id = id;
+		this.text = text;
+	}
+	
+}
